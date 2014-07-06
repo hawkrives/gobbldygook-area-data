e9094d54ca0dddaaa2b32ad06b1ca5d0db7d5085
Hawken Rives <hawkrives@gmail.com>
2014-07-06 18:46:08 -0500

    Create update-filemaps.py
    a script to add a {hash, path} dictionary for each file in this repo

---
+++ update-filemaps.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+import hashlib
+import json
+import os
+
+areasOfStudy = ['degrees', 'majors', 'concentrations']
+
+output = {}
+
+for area in areasOfStudy:
+	files = os.listdir(area)
+	output[area] = []
+	for filename in files:
+		path = area + '/' + filename 
+		with open(path, 'rb') as infile:
+			info = {
+				'path': path, 
+				'hash': hashlib.sha1(infile.read()).hexdigest()
+			}
+			output[area].append(OrderedDict(sorted(info.items())))
+	output[area] = sorted(output[area], key=lambda item: item['path'])
+
+output = OrderedDict(sorted(output.items()))
+
+with open('info.json', 'w') as outfile:
+	outfile.write(json.dumps(output, separators=(',',':')))
+
+print('Updated hashes; wrote info.json')
