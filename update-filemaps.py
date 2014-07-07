e03a712645fb18b6ec1283027400bb55141d47ae
Hawken Rives <hawkrives@gmail.com>
2014-07-06 23:19:30 -0500

    Wrapped update-filemaps in a function

--- update-filemaps.py
+++ update-filemaps.py
@@ -3,26 +3,29 @@ import hashlib
 import json
 import os
 
-areasOfStudy = ['degrees', 'majors', 'concentrations']
+def main():
+	areasOfStudy = ['degrees', 'majors', 'concentrations']
+	output = {}
 
-output = {}
+	for area in areasOfStudy:
+		files = os.listdir(area)
+		output[area] = []
+		for filename in files:
+			path = area + '/' + filename 
+			with open(path, 'rb') as infile:
+				info = {
+					'path': path, 
+					'hash': hashlib.sha1(infile.read()).hexdigest()
+				}
+				output[area].append(OrderedDict(sorted(info.items())))
+		output[area] = sorted(output[area], key=lambda item: item['path'])
 
-for area in areasOfStudy:
-	files = os.listdir(area)
-	output[area] = []
-	for filename in files:
-		path = area + '/' + filename 
-		with open(path, 'rb') as infile:
-			info = {
-				'path': path, 
-				'hash': hashlib.sha1(infile.read()).hexdigest()
-			}
-			output[area].append(OrderedDict(sorted(info.items())))
-	output[area] = sorted(output[area], key=lambda item: item['path'])
+	output = OrderedDict(sorted(output.items()))
 
-output = OrderedDict(sorted(output.items()))
+	with open('info.json', 'w') as outfile:
+		outfile.write(json.dumps(output, separators=(',',':')))
 
-with open('info.json', 'w') as outfile:
-	outfile.write(json.dumps(output, separators=(',',':')))
+	print('Hashed files; wrote info.json')
 
-print('Updated hashes; wrote info.json')
+if __name__ =  '__main__':
+	main()
