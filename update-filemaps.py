12fe4c9c2d3566b2dba9a4ac7469972c6467a364
Hawken Rives <hawkrives@gmail.com>
2014-07-08 20:48:10 -0500

    Major rework of update-filemaps to allow sharing between this and gobbldygook-data

--- update-filemaps.py
+++ update-filemaps.py
@@ -1,31 +1,46 @@
 from collections import OrderedDict
+from argparse import ArgumentParser
 import hashlib
 import json
 import os
 
-def main():
-	areasOfStudy = ['degrees', 'majors', 'concentrations']
+def json_folder_map(folders, kind):
 	output = {}
 
-	for area in areasOfStudy:
-		files = os.listdir(area)
-		output[area] = []
+	for folder_name in folders:
+		files = os.listdir(folder_name)
+		output[folder_name] = []
 		for filename in files:
-			path = area + '/' + filename 
+			path = folder_name + '/' + filename
 			with open(path, 'rb') as infile:
 				info = {
-					'path': path, 
+					'path': path,
 					'hash': hashlib.sha1(infile.read()).hexdigest()
 				}
-				output[area].append(OrderedDict(sorted(info.items())))
-		output[area] = sorted(output[area], key=lambda item: item['path'])
+				output[folder_name].append(OrderedDict(sorted(info.items())))
+		output[folder_name] = sorted(output[folder_name], key=lambda item: item['path'])
 
-	output = OrderedDict(sorted(output.items()))
+	output = OrderedDict(sorted(
+		{
+			'info': OrderedDict(sorted(output.items())), 
+			'type': kind
+		}.items()
+	))
 
 	with open('info.json', 'w') as outfile:
-		outfile.write(json.dumps(output, separators=(',',':')))
+		outfile.write(json.dumps(output, indent='\t', separators=(',', ': ')))
+		outfile.write('\n')
 
 	print('Hashed files; wrote info.json')
 
-if __name__ =  '__main__':
-	main()
+
+if __name__ == '__main__':
+	argparser = ArgumentParser()
+	argparser.add_argument('--folders', '-f', type=str, nargs='*', help='Folders to process')
+	argparser.add_argument('--kind', '-k', type=str, nargs='*', help='Kind of data')
+	args = argparser.parse_args()
+	# print(args)
+	json_folder_map(
+		folders=args.folders or ['degrees', 'majors', 'concentrations'],
+		kind=args.kind or 'areas'
+	)
