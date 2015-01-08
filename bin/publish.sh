91a6b8f07f57e070de7df5eadbe06a9b6f688adc
Hawken Rives <hawkrives@gmail.com>
2015-01-08 17:12:22 -0600

    Add two scripts

---
+++ bin/publish.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+git pull --rebase
+
+npm run build
+
+echo ""
+node ./bin/version.js
+echo ""
+
+read -p "New Version [major|minor|patch]: " version
+npm version $version
+
+npm publish
+
+git push --follow-tags
