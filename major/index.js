1b21ad3b379e7b3ca10208fba51173458f9c3f9e
Hawken Rives <hawkrives@gmail.com>
2015-01-08 14:29:18 -0600

    Move sto-areas from gobbldygook to gobbldygook-area-data

---
+++ major/index.js
@@ -0,0 +1,19 @@
+import asianStudies from './asianStudiesMajor'
+import chemistry from './chemistryMajor'
+import computerScience201415 from './computerScienceMajor-2014-15'
+import english from './englishMajor'
+import exerciseScience from './exerciseScienceMajor'
+import math from './mathMajor'
+import physics from './physicsMajor'
+import poliSci from './poliSciMajor'
+
+export {
+	asianStudies,
+	chemistry,
+	computerScience201415,
+	english,
+	exerciseScience,
+	math,
+	physics,
+	poliSci
+}
