476f1db8e21d362c78902c3cf6bcd2b825d15ece
Hawken Rives <hawkrives@gmail.com>
2015-01-09 00:15:16 -0600

    Update m-csci-2014-15.test.js

--- test/m-csci-2014-15.test.js
+++ test/m-csci-2014-15.test.js
@@ -3,97 +3,121 @@ import findArea from '../index'
 let compSci = findArea('m-csci', 2014)
 let reqs = compSci._requirements
 
-describe('Major - Computer Science (2014-15)', () => {
-	it('foundationCourses - checks for courses that fulfill the foundational requirements', () => {
+describe('Computer Science Major [2014-15]', () => {
+	describe('#foundationCourses()', () => {
 		let {foundationCourses} = compSci._requirements
-		let validCourseSet = [
-			{deptnum: 'CSCI 121'},
-			{deptnum: 'CSCI 251'},
-			{deptnum: 'CSCI 252'},
-			{deptnum: 'CSCI 241'},
-			{deptnum: 'MATH 282', term: 20141},
-		]
 
-		let expectedResult = {
-			title: 'Foundation',
-			type: 'array/boolean',
-			description: /\w/,
-			result: true,
-			details: [
-				{ title: 'CS1', result: true, },
-				{ title: 'Design', result: true, },
-				{ title: 'Proof Writing', result: true, },
-			],
-		}
+		it('checks for courses that fulfill the foundational requirements', () => {
+			let validCourseSet = [
+				{deptnum: 'CSCI 121'},
+				{deptnum: 'CSCI 251'},
+				{deptnum: 'CSCI 252'},
+				{deptnum: 'CSCI 241'},
+				{deptnum: 'MATH 282', term: 20141},
+			]
 
-		foundationCourses(validCourseSet).should.match(expectedResult)
+			let result = foundationCourses(validCourseSet)
+
+			expect(result).property('title').to.equal('Foundation')
+			expect(result).property('type').to.equal('array/boolean')
+			expect(result).property('result').to.be.true
+			expect(result).property('details').to.be.an.array
+			expect(result).deep.property('details[0].result').to.be.true
+			expect(result).deep.property('details[1].result').to.be.true
+			expect(result).deep.property('details[2].result').to.be.true
+		})
+
+		it('not all sets of courses are valid', () => {
+			let invalidCourseSet = [{deptnum: 'CSCI 121'}]
+			let result = foundationCourses(invalidCourseSet)
+			expect(result).property('result').to.be.false
+		})
 	})
 
-	it('coreCourses - checks for courses that fulfill the core requirements', () => {
+	describe('#coreCourses()', () => {
 		let {coreCourses} = compSci._requirements
-		let validCourseSet = [
-			{deptnum: 'CSCI 253'},
-			{deptnum: 'CSCI 263'},
-			{deptnum: 'CSCI 276'},
-			{deptnum: 'CSCI 300', title: 'Parallel and Distributed Computing'},
-		]
 
-		let expectedResult = {
-			title: 'Core',
-			type: 'array/boolean',
-			description: /\w/,
-			result: true,
-			details: [
-				{ title: 'Algorithms', result: true, description: /\w/, },
-				{ title: 'Ethics', result: true, description: /\w/, },
-				{ title: 'Theory', result: true, description: /\w/, },
-				{ title: 'Options', result: true, description: /\w/, },
-			],
-		}
+		it('checks for courses that fulfill the core requirements', () => {
+			let validCourseSet = [
+				{deptnum: 'CSCI 253'},
+				{deptnum: 'CSCI 263'},
+				{deptnum: 'CSCI 276'},
+				{deptnum: 'CSCI 300', title: 'Parallel and Distributed Computing'},
+			]
 
-		coreCourses(validCourseSet).should.match(expectedResult)
+			let result = coreCourses(validCourseSet)
+
+			expect(result).property('title').to.equal('Core')
+			expect(result).property('type').to.equal('array/boolean')
+			expect(result).property('result').to.be.true
+			expect(result).property('details').to.be.an.array
+			expect(result).deep.property('details[0].result').to.be.true
+			expect(result).deep.property('details[1].result').to.be.true
+			expect(result).deep.property('details[2].result').to.be.true
+			expect(result).deep.property('details[3].result').to.be.true
+		})
+
+		it('not all sets of courses are valid', () => {
+			let invalidCourseSet = [{deptnum: 'CSCI 253'}]
+			let result = coreCourses(invalidCourseSet)
+			expect(result).property('result').to.be.false
+		})
 	})
 
-	it('electiveCourses - checks for courses that fulfill the electives requirement', () => {
+	describe('#electiveCourses()', () => {
 		let {electiveCourses} = compSci._requirements
-		let validCourseSet = [
-			{depts: ['CSCI'], deptnum: 'CSCI 200'},
-			{depts: ['CSCI'], deptnum: 'CSCI 201'},
-			{depts: ['CSCI'], deptnum: 'CSCI 300'},
-		]
 
-		let expectedResult = {
-			title: 'Electives',
-			type: 'object/number',
-			description: /\w/,
-			result: true,
-			details: {
-				has: 3,
-				needs: 2,
-				matches: validCourseSet,
-			},
-		}
+		it('checks for courses that fulfill the electives requirement', () => {
+			let validCourseSet = [
+				{depts: ['CSCI'], deptnum: 'CSCI 200'},
+				{depts: ['CSCI'], deptnum: 'CSCI 201'},
+				{depts: ['CSCI'], deptnum: 'CSCI 300'},
+			]
 
-		electiveCourses(validCourseSet).should.match(expectedResult)
+			let result = electiveCourses(validCourseSet)
+
+			expect(result).property('title').to.equal('Electives')
+			expect(result).property('type').to.equal('object/number')
+			expect(result).property('result').to.be.true
+			expect(result).property('details').to.be.an.object
+			expect(result).deep.property('details.has').to.equal(3)
+			expect(result).deep.property('details.needs').to.equal(2)
+			expect(result).deep.property('details.matches').to.be.an.array
+			expect(result).deep.property('details.matches').to.have.length(3)
+		})
+
+		it('not all sets of courses are valid', () => {
+			let invalidCourseSet = [{depts: ['CSCI'], deptnum: 'CSCI 200'}]
+			let result = electiveCourses(invalidCourseSet)
+			expect(result).property('result').to.be.false
+		})
 	})
 
-	it('capstoneCourse - checks for courses that fulfill the capstone requirement', () => {
+	describe('#capstoneCourse()', () => {
 		let {capstoneCourse} = compSci._requirements
-		let validCourseSet = [
-			{depts: ['CSCI'], deptnum: 'CSCI 390'},
-		]
 
-		let expectedResult = {
-			title: 'Capstone',
-			type: 'boolean',
-			description: /\w/,
-			result: true,
-		}
+		it('checks for courses that fulfill the capstone requirement', () => {
+			let validCourseSet = [
+				{depts: ['CSCI'], deptnum: 'CSCI 390'},
+			]
 
-		capstoneCourse(validCourseSet).should.match(expectedResult)
+			let result = capstoneCourse(validCourseSet)
+
+			expect(result).property('title').to.equal('Capstone')
+			expect(result).property('type').to.equal('boolean')
+			expect(result).property('result').to.be.true
+		})
+
+		it('not all sets of courses are valid', () => {
+			let invalidCourseSet = [{depts: ['CSCI'], deptnum: 'CSCI 200'}]
+			let result = capstoneCourse(invalidCourseSet)
+			expect(result).property('result').to.be.false
+		})
 	})
 
-	it('checkComputerScienceMajor - expects a promise', () => {
-		compSci.check({then() { return {courses: []} }}).should.be.a.Promise
+	describe('#checkComputerScienceMajor()', () => {
+		it('returns a promise', () => {
+			expect(compSci.check(global.basicThenableStudent)).to.be.a.Promise
+		})
 	})
 })
--- test/support/node.js
+++ test/support/node.js
@@ -3,3 +3,5 @@ var chaiAsPromised = require('chai-as-promised')
 
 global.expect = chai.expect
 chai.use(chaiAsPromised)
+
+global.basicThenableStudent = {then() { return {courses: []} }}
