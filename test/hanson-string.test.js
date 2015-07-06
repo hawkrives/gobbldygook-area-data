97c266821a88686839556c47a4f327ab158539bc
Hawken Rives <hawkrives@gmail.com>
2015-07-06 17:29:11 +0900

    continue adding tests

--- test/hanson-string.test.js
+++ test/hanson-string.test.js
@@ -25,16 +25,6 @@ describe('parse hanson-string', () => {
             })
         })
 
-        xit('parses courses with no departments after an prior department', () => {
-            parse('CSCI 121')
-            const c2 = parse('125')
-            expect(c2).to.deep.equal({
-                $type: 'course',
-                department: ['CSCI'],
-                number: 125,
-            })
-        })
-
         it('parses courses with sections', () => {
             expect(parse('CSCI 121.A')).to.deep.equal({
                 $type: 'course',
@@ -151,6 +141,223 @@ describe('parse hanson-string', () => {
                         $type: 'course',
                         department: ['CSCI'],
                         number: 125,
+                    },
+                ],
+            })
+        })
+
+        it('parses courses seperated by & as being and-d', () => {
+            expect(parse('CSCI 121 & CSCI 125')).to.deep.equal({
+                $type: 'boolean',
+                $and: [
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 121,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 125,
+                    },
+                ],
+            })
+        })
+
+        it('parses courses with no departments after an prior department', () => {
+            expect(parse('CSCI 121 | 125')).to.deep.equal({
+                $type: 'boolean',
+                $or: [
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 121,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 125,
+                    },
+                ],
+            })
+        })
+
+        it('changes departments when given a new one', () => {
+            expect(parse('CSCI 121 | PSCI 125')).to.deep.equal({
+                $type: 'boolean',
+                $or: [
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 121,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['PSCI'],
+                        number: 125,
+                    },
+                ],
+            })
+        })
+
+        it('allows several &-d courses in a row', () => {
+            expect(parse('CSCI 121 & 125 & 126 & 123')).to.deep.equal({
+                $type: 'boolean',
+                $and: [
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 121,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 125,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 126,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 123,
+                    },
+                ],
+            })
+        })
+
+        it('allows several |-d courses in a row', () => {
+            expect(parse('CSCI 121 | 125 | 126 | 123')).to.deep.equal({
+                $type: 'boolean',
+                $or: [
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 121,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 125,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 126,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 123,
+                    },
+                ],
+            })
+        })
+
+        it('keeps duplicates in a list of courses', () => {
+            expect(parse('CSCI 121 | 121 | 125')).to.deep.equal({
+                $type: 'boolean',
+                $or: [
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 121,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 121,
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 125,
+                    },
+                ],
+            })
+        })
+
+        it('allows a & b | c – boolean logic for courses', () => {
+            expect(parse('CSCI 121 & 122 | 123')).to.deep.equal({
+                $type: 'boolean',
+                $or: [
+                    {
+                        $type: 'boolean',
+                        $and: [
+                            {
+                                $type: 'course',
+                                department: ['CSCI'],
+                                number: 121,
+                            },
+                            {
+                                $type: 'course',
+                                department: ['CSCI'],
+                                number: 122,
+                            },
+                        ],
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 123,
+                    },
+                ],
+            })
+        })
+
+        it('allows a | b & c – boolean logic for courses', () => {
+            expect(parse('CSCI 121 | 122 & 123')).to.deep.equal({
+                $type: 'boolean',
+                $or: [
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 121,
+                    },
+                    {
+                        $type: 'boolean',
+                        $and: [
+                            {
+                                $type: 'course',
+                                department: ['CSCI'],
+                                number: 122,
+                            },
+                            {
+                                $type: 'course',
+                                department: ['CSCI'],
+                                number: 123,
+                            },
+                        ],
+                    },
+                ],
+            })
+        })
+
+        it('supports parentheses to control order-of-operations - (a | b) & c', () => {
+            expect(parse('(CSCI 121 | 122) & 123')).to.deep.equal({
+                $type: 'boolean',
+                $and: [
+                    {
+                        $type: 'boolean',
+                        $or: [
+                            {
+                                $type: 'course',
+                                department: ['CSCI'],
+                                number: 121,
+                            },
+                            {
+                                $type: 'course',
+                                department: ['CSCI'],
+                                number: 122,
+                            },
+                        ],
+                    },
+                    {
+                        $type: 'course',
+                        department: ['CSCI'],
+                        number: 123,
                     },
                 ],
             })
