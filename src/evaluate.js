c292d6957ac6c6d0e5651ab5befd9eef1d83eb90
Hawken Rives <hawkrives@gmail.com>
2015-07-08 22:24:13 +0900

    rename argument in checkForCourse

--- src/evaluate.js
+++ src/evaluate.js
@@ -49,8 +49,8 @@ export function compareCourse(course, to) {
 }
 
 
-export function checkForCourse(course, courses) {
-    return any(courses, (c) => compareCourse(c, course))
+export function checkForCourse(query, courses) {
+    return any(courses, (c) => compareCourse(c, query))
 }
 
 
