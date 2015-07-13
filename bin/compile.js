4254e5fa282bdc5b2d8d01bf206f19e7f449b0fd
Hawken Rives <hawkrives@gmail.com>
2015-07-13 18:41:47 +0900

    update compile.js

--- bin/compile.js
+++ bin/compile.js
@@ -1,18 +1,8 @@
 import commander from 'commander'
-import Promise from 'bluebird'
 import {version} from '../package.json'
-import stdinCb from 'get-stdin'
-const stdin = Promise.promisify(stdinCb)
-import fs from 'graceful-fs'
-const readFile = Promise.promisify(fs.readFile)
-
+import {readFileSync} from 'graceful-fs'
 import yaml from 'js-yaml'
 import {enhanceFile} from '../src/hanson'
-
-function run(data) {
-    const mutated = enhanceFile(yaml.safeLoad(data), {topLevel: true})
-    console.log(JSON.stringify(mutated, null, 2))
-}
 
 function cli() {
     commander
@@ -22,11 +12,9 @@ function cli() {
 
     let [filename] = process.argv.slice(2)
 
-    if (filename === '-') {
-        stdin().then(run)
-    }
-    else if (filename) {
-        readFile(filename, 'utf-8').then(run)
+    if (filename) {
+        const mutated = enhanceFile(yaml.safeLoad(readFileSync(filename, 'utf-8')), {topLevel: true})
+        console.log(JSON.stringify(mutated, null, 2))
     }
     else {
         commander.outputHelp()
