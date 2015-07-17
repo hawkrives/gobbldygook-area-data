58cf4a3d95ddd553a917f9a187c5fef4e6ce8004
Hawken Rives <hawkrives@gmail.com>
2015-07-17 23:09:40 +0900

    finish up tests for computeBoolean

--- test/compute-boolean.test.js
+++ test/compute-boolean.test.js
@@ -1,8 +1,62 @@
 import {computeBoolean} from '../lib/compute-chunk'
 
 describe('computeBoolean', () => {
-    xit('computes the boolean result of and-clauses', () => {})
-    xit('computes the boolean result of or-clauses', () => {})
+    it('computes the boolean result of and-clauses', () => {
+        const clause = {
+            $type: 'boolean',
+            $and: [
+                {$type: 'course', 'department': ['CSCI'], 'number': 121},
+                {$type: 'course', 'department': ['CSCI'], 'number': 125},
+            ],
+        }
+        const requirement = {result: clause}
+        const courses = [
+            {department: ['CSCI'], number: 121},
+            {department: ['CSCI'], number: 125},
+        ]
+
+        const result = computeBoolean(clause, requirement, courses)
+        expect(clause).to.deep.equal({
+            $type: 'boolean',
+            $and: [
+                {_result: true, $type: 'course', department: ['CSCI'], number: 121},
+                {_result: true, $type: 'course', department: ['CSCI'], number: 125},
+            ],
+            _matches: [
+                {_result: true, $type: 'course', department: ['CSCI'], number: 121},
+                {_result: true, $type: 'course', department: ['CSCI'], number: 125},
+            ],
+        })
+        expect(result).to.be.true
+    })
+
+    it('computes the boolean result of or-clauses', () => {
+        const clause = {
+            $type: 'boolean',
+            $or: [
+                {$type: 'course', 'department': ['CSCI'], 'number': 121},
+                {$type: 'course', 'department': ['CSCI'], 'number': 125},
+            ],
+        }
+        const requirement = {result: clause}
+        const courses = [
+            {department: ['CSCI'], number: 121},
+            {department: ['CSCI'], number: 125},
+        ]
+
+        const result = computeBoolean(clause, requirement, courses)
+        expect(clause).to.deep.equal({
+            $type: 'boolean',
+            $or: [
+                {_result: true, $type: 'course', department: ['CSCI'], number: 121},
+                {$type: 'course', department: ['CSCI'], number: 125},
+            ],
+            _matches: [
+                {_result: true, $type: 'course', department: ['CSCI'], number: 121},
+            ],
+        })
+        expect(result).to.be.true
+    })
 
     it('only computes an or-clause until it has a result', () => {
         const clause = {
