fd736c1df3c32da352d975fdb3242525b9cd51c8
Hawken Rives <hawkrives@gmail.com>
2015-07-17 17:08:39 +0900

    whitespace inside of tests is nice

--- test/path-to-override.test.js
+++ test/path-to-override.test.js
@@ -4,9 +4,11 @@ describe('pathToOverride', () => {
     it('computes the path to an override', () => {
         expect(pathToOverride(['a', 'b', 'c d'])).to.equal('a.b.c d')
     })
+
     it('lower-cases the path', () => {
         expect(pathToOverride(['aA', 'b', 'c d'])).to.equal('aa.b.c d')
     })
+
     it('retains spaces in the path', () => {
         expect(pathToOverride(['aA', 'b', 'Studio aRt'])).to.equal('aa.b.studio art')
     })
