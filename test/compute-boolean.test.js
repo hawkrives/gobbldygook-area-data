f42c23a5c65b7079cfabab3f031fa6f27acbcff5
Hawken Rives <hawkrives@gmail.com>
2015-07-18 19:46:26 +0900

    stub out more computeBoolean and computeOr tests

--- test/compute-boolean.test.js
+++ test/compute-boolean.test.js
@@ -113,4 +113,96 @@ describe('computeBoolean', () => {
         })
         expect(result).to.be.true
     })
+
+    it('can compute the result of several other boolean expressions', () => {
+        const clause = {
+            $and: [
+                {
+                    $or: [
+                        {$type: 'course', department: ['CSCI'], number: 121},
+                        {$type: 'course', department: ['CSCI'], number: 125},
+                    ],
+                    $type: 'boolean',
+                },
+                {
+                    $or: [
+                        {$type: 'course', department: ['CSCI'], number: 130},
+                        {$type: 'course', department: ['CSCI'], number: 131},
+                    ],
+                    $type: 'boolean',
+                },
+            ],
+            $type: 'boolean',
+        }
+        const requirement = {result: clause}
+
+        const courses = [
+            {department: ['CSCI'], number: 130},
+            {department: ['CSCI'], number: 125},
+        ]
+
+        const result = computeBoolean(clause, requirement, courses)
+        expect(clause).to.deep.equal({
+            $and: [
+                {
+                    $or: [
+                        {_result: false, $type: 'course', department: ['CSCI'], number: 121},
+                        {_result: true, $type: 'course', department: ['CSCI'], number: 125},
+                    ],
+                    _matches: [{_result: true, $type: 'course', department: ['CSCI'], number: 125}],
+                    $type: 'boolean',
+                    _result: true,
+                },
+                {
+                    $or: [
+                        {_result: true, $type: 'course', department: ['CSCI'], number: 130},
+                        {$type: 'course', department: ['CSCI'], number: 131},
+                    ],
+                    _matches: [{_result: true, $type: 'course', department: ['CSCI'], number: 130}],
+                    $type: 'boolean',
+                    _result: true,
+                },
+            ],
+            $type: 'boolean',
+            _matches: [
+                {_result: true, $type: 'course', department: ['CSCI'], number: 125},
+                {_result: true, $type: 'course', department: ['CSCI'], number: 130},
+            ],
+        })
+        expect(result).to.be.true
+    })
+
+    it('can compute the result of several course expressions', () => {
+        const clause = {
+            $or: [
+                {$type: 'course', department: ['CSCI'], number: 121},
+                {$type: 'course', department: ['CSCI'], number: 125},
+            ],
+            $type: 'boolean',
+        }
+        const requirement = {result: clause}
+
+        const courses = [
+            {department: ['CSCI'], number: 130},
+            {department: ['CSCI'], number: 125},
+        ]
+
+        const result = computeBoolean(clause, requirement, courses)
+        expect(clause).to.deep.equal({
+            $or: [
+                {_result: false, $type: 'course', department: ['CSCI'], number: 121},
+                {_result: true, $type: 'course', department: ['CSCI'], number: 125},
+            ],
+            $type: 'boolean',
+            _matches: [
+                {_result: true, $type: 'course', department: ['CSCI'], number: 125},
+            ],
+        })
+        expect(result).to.be.true
+    })
+    it('can compute the result of several modifier expressions')
+    it('can compute the result of several occurrence expressions')
+    it('can compute the result of several of-expressions')
+    it('can compute the result of several requirement references')
+    it('can compute the result of several where-expressions')
 })
--- test/compute-of.test.js
+++ test/compute-of.test.js
@@ -1,1 +1,53 @@
-// of: stores the number of matches in its containing expression
+import {computeOf} from '../lib/compute-chunk'
+
+describe('computeOf', () => {
+    it('computes a list of boolean-equivalent expressions against a desired count', () => {
+        const expr = {
+            $type: 'of',
+            $count: 2,
+            $of: [
+                {$type: 'course', department: ['CSCI'], number: 121},
+                {$type: 'course', department: ['CSCI'], number: 125},
+                {$type: 'course', department: ['CSCI'], number: 150},
+            ],
+        }
+        const req = {
+            $type: 'requirement',
+            result: expr,
+        }
+
+        const courses = [
+            {department: ['CSCI'], number: 121},
+            {department: ['CSCI'], number: 125},
+        ]
+
+        const expectedResult = {
+            $type: 'of',
+            $count: 2,
+            _counted: 2,
+            _matches: [
+                {_result: true, $type: 'course', department: ['CSCI'], number: 121},
+                {_result: true, $type: 'course', department: ['CSCI'], number: 125},
+            ],
+            $of: [
+                {_result: true, $type: 'course', department: ['CSCI'], number: 121},
+                {_result: true, $type: 'course', department: ['CSCI'], number: 125},
+                {_result: false, $type: 'course', department: ['CSCI'], number: 150},
+            ],
+        }
+
+        console.log
+        expect(computeOf(expr, req, courses)).to.be.true
+        expect(expr).to.deep.equal(expectedResult)
+    })
+
+    it('stores the number of matches in its containing expression')
+
+    it('handles counting boolean expressions')
+    it('handles counting course expressions')
+    it('handles counting modifier expressions')
+    it('handles counting occurrence expressions')
+    it('handles counting nested of-expressions')
+    it('handles counting requirement references')
+    it('handles counting where-expressions')
+})
